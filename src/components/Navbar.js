876c1e6fd299c53fa7e2d2a994dd643eb48af460
Claire <clairepelletier.prod@gmail.com>
2019-10-07 11:48:02 +0200

    Navbar component ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav>
+    <nav className="Navbar">
       <a href="https://fr.wikipedia.org/wiki/Les_Simpson">The Simpsons</a>
     </nav>
   );
