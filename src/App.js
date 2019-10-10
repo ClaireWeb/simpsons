f70cd8715631395867fb63b4a64c3155eea66801
Claire <clairepelletier.prod@gmail.com>
2019-10-10 15:56:44 +0200

    DONE: rolling image with state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import QuoteList from './components/QuoteList';
 import Lamp from './Lamp';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Lamp on />
-      <Lamp />
-      <QuoteList />
-    </div>
-  );
+class App extends Component {
+    state = {
+      working: true
+    }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  }
+  render() {
+    const willWork = this.state.working ? 'stop' : 'works';
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="App-rolling">
+          <button 
+            className={willWork}
+            onClick={this.handleClick}
+          > 
+            {this.state.working ? 'CLICK ME' : 'STOOOP !'}
+          </button>
+          <div>
+            <img
+              className={willWork}
+              src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Simpsons_Donut.svg/1024px-Simpsons_Donut.svg.png'
+              alt='giant donut'
+            />
+          </div>
+        </div>
+        {/* <Lamp on />
+        <Lamp /> */}
+        <QuoteList />
+      </div>
+    )
+  }
 }
 
 export default App;
